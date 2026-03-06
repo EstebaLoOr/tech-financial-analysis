eca934a1bb57ae78801c1f77b641263274e90429
EstebaLoOr <110425819+EstebaLoOr@users.noreply.github.com>
2026-03-06 10:52:57 -0600

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,1 +1,26 @@
+-- Average Net Margin by Company
+SELECT company, AVG(net_margin) AS avg_net_margin
+FROM financials
+GROUP BY company
+ORDER BY avg_net_margin DESC;
 
+
+-- Net Margin Trend
+SELECT company, year, net_margin
+FROM financials
+WHERE company = 'NVIDIA'
+ORDER BY year ASC;
+
+
+-- Highest Net Margin per Company
+WITH highest_margin AS (
+	SELECT 
+		company, 
+        year, 
+        net_margin, 
+        ROW_NUMBER() OVER(PARTITION BY company ORDER BY net_margin DESC) AS ranking
+	FROM financials
+)
+SELECT company, year, net_margin
+FROM highest_margin
+WHERE ranking = 1;
